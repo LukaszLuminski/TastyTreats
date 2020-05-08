f5c274effa84fc7ca201b1b329786408114875bb
Lukasz Luminski <lukk87@gmail.com>
2020-05-08 20:01:00 +0100

    recaptcha added

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
-const { check, validationResult } = require('express-validator');
+const {
+  check,
+  validationResult
+} = require('express-validator');
+const req = require('request');
+const secretKey = '6LfLdvQUAAAAAJLGuXce4Ul6sHD1IOU1OPX_Y2qt';
 
-let urlencoded = bodyParser.urlencoded({extended: false});
+let urlencoded = bodyParser.urlencoded({
+  extended: false
+});
 
 app.use(bodyParser.json());
 app.use(urlencoded);
@@ -22,7 +29,9 @@ hbs.registerPartials(partialsPath);
 app.use(express.static(publicStaticDirPath));
 
 app.get('/', (request, response) => {
-  response.render('index.hbs', {title: 'Tasty Treats | Contact'});
+  response.render('index.hbs', {
+    title: 'Tasty Treats | Contact'
+  });
 });
 
 app.post('/formData', [
@@ -30,18 +39,45 @@ app.post('/formData', [
   check('email').isEmail(),
 
 ], (request, response) => {
-   const errors = validationResult(request);
 
-   if (!errors.isEmpty()){
-     return response.status(422).json({
-       errors: errors.array()
-     });
-   }
+  if (!request.body.captcha) {
+    return response.json({
+      'msg': 'Captcha token is undefined'
+    });
+  }
 
-   response.status(202).json({
-     success: 'Ok'
-   });
+  const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${request.body.captcha}`;
+
+  req(verifyUrl, (err, res, body) => {
+
+    const errors = validationResult(request);
+
+    if (err) {
+      console.log(err);
+    }
+
+    body = JSON.parse(body);
+
+    if (!body.success || body.score < 0.4) {
+      return response.json({
+        'msg': 'You might be a robot. Sorry, you are banned!',
+        'score': body.score
+      });
+    }
+
+    if (!errors.isEmpty()) {
+      return response.status(422).json({
+        errors: errors.array()
+      });
+    }
+
+   response.json({
+      'msg': 'You have been verified!',
+      'score': body.score,
+      success: 'Ok'
+    });
+  });
 
 });
 
-app.listen(port, ()=> console.log('Server running on port:' + port));
+app.listen(port, () => console.log('Server running on port:' + port));
--- public/js/client.js
+++ public/js/client.js
@@ -1,61 +1,146 @@
 //jshint esversion:6
 
 let form = document.querySelector('form');
+let errorContainer = document.querySelector('.errorContainer');
+let successContainer = document.querySelector('.successContainer');
 
-form.onsubmit = sendData;
+// form.onsubmit = sendData;
+form.onsubmit = runVerify;
 
-function sendData(e) {
+
+function runVerify(e) {
   e.preventDefault();
+  runCaptcha();
+}
+
+function runCaptcha() {
+  grecaptcha.execute('6LfLdvQUAAAAAGmo47NQbb4cdV73OxY2r7DmSLBj', {
+    action: 'index'
+  }).then(function(token) {
+
+    const captcha = token;
+
+    sendToken(captcha);
+  });
+}
+
+
+
+function sendToken(captcha) {
 
   let formData = new FormData(form);
 
-  let Params = {
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: formData.get('name'),
-      email: formData.get('email'),
-      message: formData.get('message'),
-      signUp: formData.get('signUp')
-    }),
-    method: "POST"
-  };
+  const info = JSON.stringify({
+    name: formData.get('name'),
+    email: formData.get('email'),
+    message: formData.get('message'),
+    signUp: formData.get('signUp'),
+    captcha: captcha
+  });
 
-  fetch('http://localhost:3000/formData', Params)
-  .then(response => response.json())
-  .then(data => {
-    $('input[type="text"], textarea').val('');
+  fetch('/formData', {
+
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-type': 'application/json'
+    },
+    body: info
+  }).then((response) => response.json())
+  .then((data)=>{
+
+  $('input[type="text"], textarea').val('');
 
     if (data.success === 'Ok') {
       console.log('Successful!');
 
-      console.log(data);
-      console.log(Params.body);
-
-      let success = document.querySelector('.success');
+    let success = document.querySelector('.success');
 
       success.innerHTML = '';
 
-      document.querySelector('.successContainer').style.display = "block";
+      errorContainer.style.display = "none";
+      successContainer.style.display = "block";
 
       let msg = 'You have successfully submitted the form!';
       success.innerHTML = `<li>${msg}</li>`;
+
+      alert('msg: ' + data.msg + ', score ' + data.score);
+
+      console.log(data);
+      // console.log(Params.body);
 
     } else {
       let error = document.querySelector('.error');
 
       error.innerHTML = '';
 
-      document.querySelector('.successContainer').style.display = "none";
+      successContainer.style.display = "none";
 
-      document.querySelector('.errorContainer').style.display = "block";
+      errorContainer.style.display = "block";
 
       data.errors.forEach(function(err) {
         error.innerHTML = `<li>${err.msg}</li>`;
       });
       console.log(data);
     }
-  })
-  .catch(err => console.log(err));
+  }).catch(err => console.log(err));
 }
+
+// function sendData(e) {
+//   e.preventDefault();
+//
+//   let formData = new FormData(form);
+//
+//   let Params = {
+//     headers: {
+// 'Accept': 'application/json, text/plain, */*',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       name: formData.get('name'),
+//       email: formData.get('email'),
+//       message: formData.get('message'),
+//       signUp: formData.get('signUp'),
+//     }),
+//     method: "POST"
+//   };
+//
+//   fetch('http://localhost:3000/formData', Params)
+//     .then(response => response.json())
+//     .then(data => {
+//
+//       $('input[type="text"], textarea').val('');
+//
+//       if (data.success === 'Ok') {
+//         console.log('Successful!');
+//
+//         console.log(data);
+//         console.log(Params.body);
+//
+//         let success = document.querySelector('.success');
+//
+//         success.innerHTML = '';
+//
+//         errorContainer.style.display = "none";
+//         successContainer.style.display = "block";
+//
+//         let msg = 'You have successfully submitted the form!';
+//         success.innerHTML = `<li>${msg}</li>`;
+//
+//       } else {
+//         let error = document.querySelector('.error');
+//
+//         error.innerHTML = '';
+//
+//         successContainer.style.display = "none";
+//
+//         errorContainer.style.display = "block";
+//
+//         data.errors.forEach(function(err) {
+//           error.innerHTML = `<li>${err.msg}</li>`;
+//         });
+//         console.log(data);
+//       }
+//     })
+//     .catch(err => console.log(err));
+// }
