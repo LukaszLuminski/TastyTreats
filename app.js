79793ffd97683510825632c99a3c4a073e4856aa
Lukasz Luminski <lukk87@gmail.com>
2020-05-10 15:29:15 +0100

    heroku fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const date = require('./date.js');
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.mongodb_uri || 'mongodb://localhost:27017/tasty-treats', {
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/tasty-treats', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
